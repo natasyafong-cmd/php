c730799eb6464b8c9bbf6ff49cce23cf9e7f5aaf
natasya <natasyafong@gmail.com>
2026-04-09 10:15:44 +0700

    Update login_check.php

--- A1133335＿馮宥芯＿作業2/login_check.php
+++ A1133335＿馮宥芯＿作業2/login_check.php
@@ -2,7 +2,7 @@
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-if ($username == "admin" && $password == "1234") {
+if ($username == "natasya" && $password == "0309") {
     header("Location: A1133335_馮宥芯_作業1.php");
     exit(); 
 } else {
