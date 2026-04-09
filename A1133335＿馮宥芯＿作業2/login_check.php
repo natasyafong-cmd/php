929e45bc8749505235848e4e76b7ff745b656675
natasya <natasyafong@gmail.com>
2026-04-09 10:24:18 +0700

    Add files via upload

---
+++ A1133335＿馮宥芯＿作業2/login_check.php
@@ -0,0 +1,12 @@
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if ($username == "natasya" && $password == "0309") {
+    header("Location: A1133335_馮宥芯_作業1.php");
+    exit(); 
+} else {
+    header("Location: login.php?error=1"); 
+    exit();
+}
+?>
